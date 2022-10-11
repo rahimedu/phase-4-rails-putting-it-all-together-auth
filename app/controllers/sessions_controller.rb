e53edc3c161d58b0979ad8af9cf6c403cf360b55
rahimedu <edwin.duche82@gmail.com>
2022-10-10 23:24:36 -0500

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+    def create
+      user = User.find_by!(username: params[:username])
+      if user
+        if user.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          # byebug
+          render json: {errors: user.errors.full_messages}, status: :unauthorized
+        end
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      # byebug
+      render json: { errors: [] }, status: :unauthorized
+    end
+  
+    def destroy
+      if session[:user_id]
+        session.delete :user_id
+        head :no_content
+      else
+        render json: { errors: []}, status: :unauthorized
+      end
+    end
+  end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+    def create
+      # byebug
+      user = User.new(user_params)
+      if user.valid? && params[:password] == params[:password_confirmation]
+        user.save!
+        session[:user_id] = user.id
+        render json: user, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def show
+      if session[:user_id]
+        user = User.find(session[:user_id])
+        render json: user
+      else
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:username, :password, :password_confirmation, :password_digest, :image_url, :bio)
+    end
+  end
